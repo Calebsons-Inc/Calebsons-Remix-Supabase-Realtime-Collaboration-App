2bc8190cdbc6697bb70dd05ff8736b4e5986f153
Caleb Thompson <caleb.softdev@gmail.com>
2026-04-17 02:30:01 -0700

    First minimum working version

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/**
+ * NOTE: When the Remix Vite plugin is active, Remix does NOT read this file.
+ * Options are passed to `vitePlugin(...)` in `vite.config.ts` instead.
+ * This file is kept for reference / compatibility with non-Vite tooling.
+ *
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+export default {
+  serverModuleFormat: "esm",
+  future: { v3_routeConvention: true },
+};
